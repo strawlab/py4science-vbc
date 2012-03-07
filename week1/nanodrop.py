28247c82a91fc80966bc461fc74071d5dad40f95
John Stowers <john.stowers@gmail.com>
2012-03-07 16:14:04 +0100

    example to plot absorbtion spectra

---
+++ week1/nanodrop.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import getfile
+
+STATE_SKIP, _, STATE_SPEC_NAME, STATE_SPEC_DATE, _, STATE_DATA = range(6)
+FREQ_FROM, FREQ_TO = 190, 841
+
+spectrum_names = []
+def func():
+  state = STATE_SPEC_NAME
+  for line in open(getfile.get_from_strawlab("week1/nanodrop-spectra.tsv")):
+      if state == STATE_SPEC_NAME:
+          spectrum_names.append(line.strip())
+      elif state == STATE_DATA:
+          wavelength, absorbance = line.strip().split()
+          yield absorbance
+          if int(wavelength) == (FREQ_TO-1):
+              state = STATE_SKIP
+          continue
+      state += 1
+data = np.fromiter(func(), float)
+
+data.resize(len(spectrum_names),FREQ_TO - FREQ_FROM)
+data = data.transpose()
+
+plt.plot(np.arange(FREQ_FROM, FREQ_TO),data)
+plt.legend(spectrum_names, loc=4)
+plt.show()
+
