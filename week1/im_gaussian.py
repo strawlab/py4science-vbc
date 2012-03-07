121900991352ac138fb8c2a39609dbd47d1725c4
John Stowers <john.stowers@gmail.com>
2012-03-07 16:39:08 +0100

    im_gaussian uses getfile

--- week1/im_gaussian.py
+++ week1/im_gaussian.py
@@ -1,14 +1,11 @@
-#!/usr/bin/python
-#
-# gaussian-blur an image 
-#
-# usage: python im_gaussian.py filename
-#
-
 import cv
 import sys
 
-img = cv.LoadImageM(sys.argv[1])
+import getfile
+
+lena = "lena-color.png"
+
+img = cv.LoadImageM(getfile.get_from_strawlab("week1/" + lena))
 cv.Smooth(img, img, smoothtype=cv.CV_GAUSSIAN, param1=11)
-cv.SaveImage("smoothed_"+sys.argv[1], img)
+cv.SaveImage("smoothed-"+lena, img)
 
