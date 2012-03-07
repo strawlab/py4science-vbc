6ed79368dedb88db273ca5d39ec03e3b8979fe38
Andreas Poehlmann <mail@andreaspoehlmann.de>
2012-03-07 16:32:19 +0100

    removed if __name__ ...

--- week1/lsm_display.py
+++ week1/lsm_display.py
@@ -4,20 +4,19 @@ import pylsm.lsmreader as lsm
 
 import getfile
 
-if __name__ == "__main__":
-    lsmfile = lsm.Lsmimage(getfile.get_from_strawlab("week1/DB331-brain.bin"))
-    lsmfile.open()
+lsmfile = lsm.Lsmimage(getfile.get_from_strawlab("week1/DB331-brain.bin"))
+lsmfile.open()
 
-    Z = lsmfile.header['CZ LSM info']['Dimension Z']
+Z = lsmfile.header['CZ LSM info']['Dimension Z']
 
-    fig = plt.figure()
-    layer = lsmfile.get_image(stack=0, channel=0)
-    im = plt.imshow(layer, cmap=plt.cm.hot)
+fig = plt.figure()
+layer = lsmfile.get_image(stack=0, channel=0)
+im = plt.imshow(layer, cmap=plt.cm.hot)
 
-    def updatefig(frame, i, Z, lsmfile):
-        i[0] = i[0]+1 if i[0] < Z-1 else 0
-        im.set_array(lsmfile.get_image(stack=i[0], channel=0))
-        return im,
+def updatefig(frame, i, Z, lsmfile):
+    i[0] = i[0]+1 if i[0] < Z-1 else 0
+    im.set_array(lsmfile.get_image(stack=i[0], channel=0))
+    return im,
 
-    ani = anim.FuncAnimation(fig, updatefig, fargs=([0],Z,lsmfile), interval=50, blit=True) 
-    plt.show()
+ani = anim.FuncAnimation(fig, updatefig, fargs=([0],Z,lsmfile), interval=50, blit=True) 
+plt.show()
