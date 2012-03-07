0ba65a87b6529e65423b8291bf53cfcaf08c916f
John Stowers <john.stowers@gmail.com>
2012-03-07 20:08:35 +0100

    tidy webscrape a little

--- week1/webscrape.py
+++ week1/webscrape.py
@@ -1,12 +1,12 @@
 import urllib2
-from BeautifulSoup import BeautifulSoup
+import BeautifulSoup
 
-soup = BeautifulSoup(
+soup = BeautifulSoup.BeautifulSoup(
           urllib2.urlopen('http://seminars.viennabiocenter.org/seminars.php').read(),
           convertEntities='html')
 for seminar in soup('table',{'width':'700'}):
     title, body = seminar('td')
-    title = ":".join( title.text.split(':')[1:] )
+    title = title.text.replace("VBC Regular Seminar:","")
     date = body('b')[0].text
 
     print "%s \n\t - %s" % (title, date)
