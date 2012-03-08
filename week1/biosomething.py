534c50201ada5fd558b6638437e7b33fd728ca20
John Stowers <john.stowers@gmail.com>
2012-03-08 22:57:32 +0100

    biosomething something

--- week1/biosomething.py
+++ week1/biosomething.py
@@ -3,8 +3,6 @@ import subprocess
 from Bio import Entrez, SeqIO, AlignIO
 from Bio.Emboss.Applications import NeedleCommandline
 
-import getfile
-
 Entrez.email = "stowers@imp.ac.at"
 
 handle = Entrez.efetch(db="nucleotide", id="AF182035",rettype="gb", retmode="text")
@@ -16,10 +14,9 @@ drosophila = SeqIO.read(handle, "genbank")
 open("homo.fasta","w").write(homo.format("fasta"))
 open("drosophila.fasta","w").write(drosophila.format("fasta"))
 
-n = NeedleCommandline(asequence="homo.fasta", bsequence="drosophila.fasta",gapopen=10, gapextend=0.5, outfile="needle.txt")
+cmdline = NeedleCommandline(asequence="homo.fasta", bsequence="drosophila.fasta",gapopen=10, gapextend=0.5, outfile="needle.txt")
+subprocess.call(str(cmdline), shell=True)
 
-print n
-#!needle -outfile=needle.txt -asequence=homo.fasta -bsequence=drosophila.fasta -gapopen=10 -gapextend=0.5
-#align = AlignIO.read("needle.txt", "emboss")
-#print align
+align = AlignIO.read("needle.txt", "emboss")
+print align
 
