2d64b8eaa33e3ffc13e13e596b3f9e372c159d60
John Stowers <john.stowers@gmail.com>
2012-03-09 11:25:19 +0100

    fix exfft to be more interesting

--- week1/exfft.py
+++ week1/exfft.py
@@ -5,6 +5,20 @@ from scipy.io import wavfile
 from getfile import get_from_strawlab
 
 rate,data = wavfile.read(get_from_strawlab("week1/sirentone.wav"))
+left = data[:,1]
 
+nsamps = len(left)
+t = np.arange(nsamps, dtype=float) / rate
+
+plt.plot(t, left) #plot in time domain
+
+xfreq = np.fft.fft(left)
+fft_freqs = np.fft.fftfreq(nsamps, d=1./rate)
+
+plt.figure()      #plot in freq domain
+plt.loglog(fft_freqs[0:nsamps/2], np.abs(xfreq)[0:nsamps/2])
+
+plt.figure()
 plt.specgram(data[:,1])
+
 plt.show()
