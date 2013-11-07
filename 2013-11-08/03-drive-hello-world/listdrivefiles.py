2853fca8fbe04f11984707e441c3fe561f621050
ap-- <mail@andreaspoehlmann.de>
2013-11-07 18:39:33 +0100

    example03

---
+++ 2013-11-08/03-drive-hello-world/listdrivefiles.py
@@ -0,0 +1,18 @@
+
+# ....
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from apgooglelayer.oauth import get_drive_service
+from apgooglelayer.drive import GoogleDrive
+
+
+secrets = "./drive-client-secrets.json"
+service = get_drive_service(secrets, "py4science-example03")
+
+Drive = GoogleDrive(service)
+
+FileTree = Drive.folder_structure()
+for name, treenode in FileTree.flatten_names():
+    print name
+
