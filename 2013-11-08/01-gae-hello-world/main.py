5ac1a4284b813330d1f1d749dc051a8c16309ed6
ap-- <mail@andreaspoehlmann.de>
2013-11-07 17:40:10 +0100

    example01

---
+++ 2013-11-08/01-gae-hello-world/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import webapp2
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
+
+
+application = webapp2.WSGIApplication([
+                                        ('/', MainPage),
+                                      ], debug=True)
+
